d3f575dc30f3f96bddcb6134f832e38b2560e0b3
bochain <bochainwu@gmail.com>
2023-10-31 00:34:31 +0800

    part 2 prog2

---
+++ homework_2/part_2/prog2/main.s
@@ -0,0 +1,15 @@
+		AREA    prog1, CODE, READONLY
+		ENTRY
+		 
+		MOV	 r3, #0x40000000
+		MOV	 r4, #0x60
+		
+		STR  r6, [r3, #8]
+		STRB r7, [r3], #12
+		LDRH r5, [r3], #12
+		LDR  r12, [r3, #4]!
+		LDR  r6, [r3, r4, ROR #28]! 
+		LDR  r0, [r3, r4, LSL #2]
+stop    B    stop
+
+        END
